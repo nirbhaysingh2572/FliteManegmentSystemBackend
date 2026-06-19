ac33baf514559db9d047b0084b7d3e96d5b66b32
nirbhaysingh2572 <chhivalaha499@gmail.com>
2026-06-19 17:02:36 +0530

    added BookingService added sending mail on compleating booking in booking service !

--- Bookingservice/src/services/booking-service.js
+++ Bookingservice/src/services/booking-service.js
@@ -3,7 +3,11 @@ const axios = require('axios');
 const { BookingRepository } = require('../repository/index');
 const { ServiceError, ValidationError } = require('../utils/errors/index');
 const { FLIGHT_SERVICE_PATH, USER_SERVICE_PATH } = require('../config/server-config');
+const { getChannel, publishMessage } =  require('../utils/ampq');
 
+
+const REMINDER_EXCHANGE = "REMINDER";
+const BOOKING_BINDING_KEY = "booking";
 
 const bookingRepository = new BookingRepository();
 
@@ -29,6 +33,11 @@ class BookingService{
             // now create the booking
             const booking = await bookingRepository.create(data);
             // after you have to add payment gateway and make booking successfuly and cancel acc..
+
+            // get notifie to user that booking were created
+            const channel = getChannel();
+            const message = "booking completed";
+            await publishMessage(channel, REMINDER_EXCHANGE, BOOKING_BINDING_KEY, message);
             return booking;
         }
         catch(error){
@@ -48,7 +57,7 @@ class BookingService{
                 // if not get response then it shuoud be connection error
                 console.log("anable to connect with other service !");
             }
-            console.log(error);
+
             console.log("some error in service Layer");
             throw(
                 new ServiceError()
